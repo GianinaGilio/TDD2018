ec6c96a1de90f2a443ef7145f4f90b9303048ccc
GianinaGilio <gianinagilio@gmail.com>
2018-09-18 17:01:49 -0300

    Creamos la funcion obtenerCantidad()

--- src/Mazo.php
+++ src/Mazo.php
@@ -4,6 +4,11 @@ namespace TDD;
 
 class Mazo {
 
+  protected cantCartas;
+
+  public function obtenerCantidad(){
+	return $this->cantCartas;
+}
   public function mezclar() {
     return TRUE;
   }
