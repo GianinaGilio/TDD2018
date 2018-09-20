1d873ba407e579df9ecd47d39cfc28fefd9dbae1
GianinaGilio <gianinagilo@gmail.com>
2018-09-20 17:58:48 -0300

    Creo el constructor de la clase carta

--- src/Carta.php
+++ src/Carta.php
@@ -4,5 +4,16 @@ namespace TDD;
 
 class Carta {
 
+	protected $numero;
+	protected $palo;
+
+	public function __construct($numero,$palo){
+	$this->numero = $numero;
+	$this->palo = $palo;
+	}
+	
+	public function verPalo(){
+	
+	}
 
  }
