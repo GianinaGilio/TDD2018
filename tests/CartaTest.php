87215217db9e64444aea933f3f55f7a147ba28bc
Sofiamonza <40038825+Sofiamonza@users.noreply.github.com>
2018-10-23 20:38:50 -0300

    Agrego otra linea al test TipoCarta

--- tests/CartaTest.php
+++ tests/CartaTest.php
@@ -10,24 +10,25 @@ class CartaTest extends TestCase {
      * Valida que se puedan crear cartas.
      */
     public function testExiste() {
-        $carta = new Carta("9","ORO");
+        $carta = new Carta("9","ORO", "ESPAÑOLA");
         $this->assertTrue(isset($carta));
     }
 
 	public function testVerNumero(){
-		$carta = new Carta("5","COPA");
+		$carta = new Carta("5","COPA", "ESPAÑOLA");
 		$this->assertEquals($carta->verNumero(),5);
 	}
 	
     public function testVerPalo(){
-		$carta = new Carta("9","ORO");
+		$carta = new Carta("9","ORO", "ESPAÑOLA");
 		$this->assertEquals($carta->verPalo(),"ORO");
 	}
 
    public function testTipoCarta(){
 		$carta = new Carta("9", "ORO", "ESPAÑOLA");
-		$carta2 = new Carta("9", "CORAZONES", "Española");
-		$this->assertEquals($carta->verTipo(),"Española");
+		$carta2 = new Carta("AS", "CORAZONES", "POKER");
+		$this->assertEquals($carta->verTipo(),"ESPAÑOLA");
+		$this->assertEquals($carta2->verTipo(),"POKER");
 	}
 	
 }
