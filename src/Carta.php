374da2dbcd5b1508ebd0be18a8a3c53f2c8d6f6e
GianinaGilio <gianinagilio@gmail.com>
2018-11-19 21:28:35 -0300

    agrego documentacion correspondiente a Carta.php

--- src/Carta.php
+++ src/Carta.php
@@ -14,10 +14,18 @@ class Carta {
 	$this->tipo = $tipo;
 	}
 	
+	/*
+	carta->int
+	Devuelve el numero de la carta.
+	*/
 	public function verNumero(){
 	return $this->numero;
 	}
 	
+	/*
+	carta->string
+	Devuelve el palo de la carta.
+	*/
 	public function verPalo(){
 	return $this->palo;
 	}
